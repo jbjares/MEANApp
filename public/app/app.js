a63abb919f3992590099a3f0deb8dcf92f859990
jbjares <joao.jares@insight-centre.org>
2015-04-03 03:20:17 +0100

    Creating a Walking Skeleton - Creating the Angular Application

---
+++ public/app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jbjares on 03/04/2015.
+ */
+angular.module('app',['ngResource','ngRoute']);
+
+angular.module('app').config(function($routeProvider,$locationProvider){
+    $locationProvider.html5Mode(true);
+    $routeProvider
+        .when('/',{templateUrl:'/partials/main',controller:'mainCtrl'});
+
+
+});
+
+angular.module('app').controller('mainCtrl',function($scope){
+    $scope.myVar = "Hello Angular";
+});
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+var app = express();
+
+function compile(str,path){
+    return stylus(str).set('filename',path);
+}
+
+app.set('views',__dirname+'/server/views');
+app.set('view engine','jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware({
+    src:__dirname+'/public',
+    compile: compile
+
+}
+));
+app.use(express.static(__dirname+'/public'));
+
+app.get('/partials/:partialPath', function (request,response) {
+    console.log('teste: '+request.params.partialPath);
+    response.render('partials/',+request.params.partialPath);
+})
+
+app.get('*',function(request,response){
+    console.log('*')
+    response.render('index');
+});
+
+
+
+var port = 3030;
+app.listen(port);
+console.log('Listening on port '+port+'...');
