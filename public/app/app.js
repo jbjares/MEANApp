2cd161ec08bb20723a5b7f465fd84f9ba4b55ff6
jbjares <joao.jares@insight-centre.org>
2015-04-04 07:13:54 +0100

    ready to commit

--- public/app/app.js
+++ public/app/app.js
@@ -4,7 +4,11 @@
 angular.module('app',['ngResource','ngRoute']);
 
 angular.module('app').config(function($routeProvider,$locationProvider){
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+
     $routeProvider
         .when('/',{templateUrl:'/partials/main',controller:'mainCtrl'});
 
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var stylus = require('stylus');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log('env: '+env);
 var app = express();
 
 function compile(str,path){
@@ -23,18 +25,39 @@ app.use(stylus.middleware({
 ));
 app.use(express.static(__dirname+'/public'));
 
+if(env==='development'){
+    console.log('dev db');
+    mongoose.connect('mongodb://localhost/multivision');
+}else{
+    console.log('prod db');
+    mongoose.connect('mongodb://jbjares:multivision@ds061371.mongolab.com:61371/multivision_jbjares');
+}
+
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error!'));
+db.once('open',function callback(){
+    console.log('db opened...');
+});
+var MessageModel = mongoose.model('Message',{message:String});
+var mongoMessage;
+MessageModel.findOne().exec(function(err,messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function (request,response) {
-    console.log('teste: '+request.params.partialPath);
-    response.render('partials/',+request.params.partialPath);
+    console.log('partialPath log: '+request.params.partialPath);
+    response.render('partials/'+request.params.partialPath);
 })
 
 app.get('*',function(request,response){
-    console.log('*')
-    response.render('index');
+    console.log('*');
+    response.render('index',
+        {mongoMessage:mongoMessage}
+    );
 });
 
 
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port '+port+'...');
